cfe1caba3cc561d7d12dd5302397a76fab6f6f89
veortiz1 <veortiz1@asu.edu>
2025-01-22 10:01:06 -0700

    install dependencies

--- routes/login.js
+++ routes/login.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const bcrypt = require('bcrypt');
 const db = require('../config/db');
-const client = require('../config/reddis');
+const client = require('../config/redis');
 const crypto = require('crypto');
 
 
